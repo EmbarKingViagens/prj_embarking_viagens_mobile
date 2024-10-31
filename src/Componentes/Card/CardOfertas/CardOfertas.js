9220478b67cadceac1f4cc1631a122ff9abb9cf8
jennifer cristine <jennifer.cristine11@gmail.com>
2024-10-31 10:45:39 -0300

    otimizando estilização card ofertas

--- src/Componentes/Card/CardOfertas/CardOfertas.js
+++ src/Componentes/Card/CardOfertas/CardOfertas.js
@@ -5,51 +5,38 @@ import { Icon } from 'react-native-elements';
 export default function CardOfertas(props) {
   return (
     <>
-      <View
-        style={{
-          width: "80%",
-          backgroundColor: '#fff',
-          borderBottomWidth: 10,
-          borderRadius: 10,
-          padding: 5,
-          overflow: 'hidden',
-          borderBottomColor: props.color,
-        }}>
-        <Text
-          style={{
-            fontSize: 20,
-            color: props.color,
-            fontWeight: 'bold',
-            textAlign: 'center',
-            margin: 3,
-          }}>
+      <View style={[styles.cardOfertas, { borderBottomColor: props.color }]}>
+        <Text style={[styles.titulo, { color: props.color }]}>
           {props.nomeOferta}
         </Text>
-        <View style={{ flexDirection: 'row' }}>
+        <View style={styles.divisor}>
           <View>
             <View style={styles.tempoOfertas}>
-              <Text
-                style={{
-                  color: props.color,
-                  fontSize: 20,
-                  textAlign: 'center',
-                }}>
+        <Text style={[styles.subtitulo, { color: props.color }]}>
+      
                 {props.txtTempoOfertas}
               </Text>
             </View>
-            <View
-              style={{
-                width: '65%',
-               
-                backgroundColor: props.color,
-                padding: 4,
-                borderBottomLeftRadius: 5,
-                borderBottomRightRadius: 5,
-              }}>
+             <View style={[styles.incluindo, { backgroundColor: props.color }]}>
               <View style={styles.icons}>
-                <Icon name="hotel" style={styles.icon} type="font-awesome-5" color="#fff" />
-                <Icon style={styles.icon}  name="mug-hot" type="font-awesome-5" color="#fff" />
-                <Icon style={styles.icon}  name="lock" type="font-awesome-5" color="#fff" />
+                <Icon
+                  name="hotel"
+                  style={styles.icon}
+                  type="font-awesome-5"
+                  color="#fff"
+                />
+                <Icon
+                  style={styles.icon}
+                  name="mug-hot"
+                  type="font-awesome-5"
+                  color="#fff"
+                />
+                <Icon
+                  style={styles.icon}
+                  name="lock"
+                  type="font-awesome-5"
+                  color="#fff"
+                />
               </View>
               <View style={styles.txtIcons}>
                 <Text style={styles.txtIncluindo}>{props.txtIncluindo1}</Text>
--- src/Componentes/Card/CardOfertas/estiloCardOfertas.js
+++ src/Componentes/Card/CardOfertas/estiloCardOfertas.js
@@ -1,65 +1,61 @@
-import { StyleSheet } from "react-native"
-import {
-    pal1,
-    pal2,
-    pal3,
-    pal4,
-    pal5,
-
-} from "../../../estilo"
+import { StyleSheet } from 'react-native';
+import { pal1, pal2, pal3, pal4, pal5 } from '../../../estilo';
 
 export default StyleSheet.create({
-    cardOfertas: {
-      
-       backgroundColor: '#fff',
-       borderBottomWidth: 10,
-       borderRadius: 10,
-       padding: 5,
-       overflow:"hidden",
-       borderBottomColor: pal1
-    },
-    tempoOfertas:{
-        
-        width: "65%",
-        
-        backgroundColor: pal5,
-        padding: 4,
-        borderTopLeftRadius: 5,
-        borderTopRightRadius: 5,
-        
-        
-    },
- 
-    incluindo:{
-        width: "85%",
-        flexDirection:"row",
-        backgroundColor:pal1,
-        padding: 4,
-       
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius: 5,
+  cardOfertas: {
+    width: '80%',
+    backgroundColor: '#fff',
+    borderBottomWidth: 10,
+    borderRadius: 10,
+    padding: 5,
+    overflow: 'hidden',
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 3,
+  },
+  subtitulo: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  tempoOfertas: {
+    width: '65%',
+    backgroundColor: pal5,
+    padding: 4,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
 
-    },
-    txtIncluindo:{
-        color:"#fff",
-        margin: 3,
-        fontSize: 10
-    },
-    imgOferta:{
-        width: 100,
-        height: 100,
-        borderRadius: 5,
-        margin:0
-    },
-    icons:{
-      flexDirection:"row",
-      margin: 3
-    },
-    txtIcons:{
-      flexDirection:"row"
-    },
-   icon:{
-     margin: 3
-   }
-
+  incluindo: {
+    width: '65%',
+    padding: 4,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  txtIncluindo: {
+    color: '#fff',
+    margin: 3,
+    fontSize: 10,
+  },
+  imgOferta: {
+    width: 100,
+    height: 100,
+    borderRadius: 5,
+    margin: 0,
+  },
+  icons: {
+    flexDirection: 'row',
+    margin: 3,
+  },
+  txtIcons: {
+    flexDirection: 'row',
+  },
+  icon: {
+    margin: 3,
+  },
+  divisor: {
+    flexDirection: 'row',
+  },
 });
